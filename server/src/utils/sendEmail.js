1c6daba3deb604043be9e587fce8d99cfd4a7273
gurusravya16 <guruzadasravya@gmail.com>
2026-05-17 23:35:16 +0530

    Update sendEmail.js

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -2,12 +2,13 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
-  secure: Number(process.env.SMTP_PORT) === 465,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+
 });
 
 export async function sendEmail({ to, subject, text, html }) {
